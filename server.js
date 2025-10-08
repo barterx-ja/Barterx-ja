3a9d06968909089ece890cfe46a55a3e98c078a7
barterx-ja <robertcgscott@yahoo.com>
2025-10-08 00:18:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -94,5 +94,4 @@ app.post('/trades/:id/status', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
