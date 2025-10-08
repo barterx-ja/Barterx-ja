dd19ca5a5ef3e247cf2c2d755b5f284185269464
barterx-ja <robertcgscott@yahoo.com>
2025-10-08 00:48:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -101,4 +101,17 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+// Serve frontend files
+app.use(express.static(__dirname));
+
+// Default route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
